bed05f4ac42ec594100c8b3c858fd9453a2cd0bb
jarey <jose.angel.rey.linares@gmail.com>
2017-07-11 18:56:01 +0200

    Actualización de los scripts de compilación y despliegue a node scripts para soporte multiplataforma.

---
+++ scripts/sandbox.js
@@ -0,0 +1,7 @@
+let shell = require('shelljs');
+
+shell.cp ('./node_modules/pdfjs-dist/build/pdf.js', './shared/');
+shell.cp ('./node_modules/pdfjs-dist/build/pdf.worker.js', './shared/');
+shell.cp ('./node_modules/pdfjs-dist/web/pdf_viewer.js ', './shared/');
+shell.cp ('./node_modules/pdfjs-dist/web/pdf_viewer.css ', './shared/');
+shell.cp('-rf', 'shared/', 'sandbox/shared');
